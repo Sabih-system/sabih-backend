ba5a98eb89e47aaad6adf4db5f9060a3edc3d0c9
amanuelcm27 <amanuelfirew27@gmail.com>
2025-06-24 19:17:42 +0300

    view, delete , update functionality for messages done with controlled access

--- communication/api/views.py
+++ communication/api/views.py
@@ -6,7 +6,7 @@ from .serializers import MessageSerializer
 from rest_framework.permissions import BasePermission
 from Team.models import Employee
 from django.db.models import Q
-
+from rest_framework.exceptions import PermissionDenied
 class IsClientOrSupervisor(BasePermission):
     def has_permission(self, request, view):
         try:
@@ -27,10 +27,6 @@ class MessageListCreateView(generics.ListCreateAPIView):
         user = self.request.user
         with_client_id = self.request.query_params.get("with_client")
         with_employee_id = self.request.query_params.get("with_employee")
-        print(f"with_client_id: {with_client_id}, with_employee_id: {with_employee_id}")
-        print("User:", user)
-        print("Has client:", hasattr(user, 'client'))
-        print("Has employee:", hasattr(user, 'employee'))
 
         if hasattr(user, 'client') and with_employee_id:
             return Message.objects.filter(
@@ -43,7 +39,6 @@ class MessageListCreateView(generics.ListCreateAPIView):
                 Q(sender_employee=user.employee, receiver_client__id=with_client_id) |
                 Q(sender_client__id=with_client_id, receiver_employee=user.employee)
             ).order_by('timestamp')
-        print("No client or employee found for user.")
         return Message.objects.none()
 
 
@@ -61,13 +56,46 @@ class MessageDetailView(generics.RetrieveAPIView):
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated, IsClientOrSupervisor]
 
+    def get_object(self):
+        user = self.request.user
+        message = super().get_object()
+
+        if ((hasattr(user, 'client') and (message.sender_client == user.client or message.receiver_client == user.client)) or
+            (hasattr(user, 'employee') and (message.sender_employee == user.employee or message.receiver_employee == user.employee))):
+            return message
+        else:
+            raise PermissionDenied("You do not have permission to view this message.")
 
 class MessageUpdateView(generics.UpdateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated, IsClientOrSupervisor]
+    
+    def perform_update(self , serializer):
+        user = self.request.user
+        print('serializer instance', serializer.instance.sender_client, serializer.instance.sender_employee)
+        if hasattr(user, 'client') and serializer.instance.sender_client == user.client:
+            serializer.save()
+        elif hasattr(user, 'employee') and serializer.instance.sender_employee == user.employee:
+            serializer.save()
+        else:
+            raise PermissionDenied("You do not have permission to update this message.")
 
 class MessageDeleteView(generics.DestroyAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated, IsClientOrSupervisor]
+    
+    def perform_destroy(self, instance):
+        user = self.request.user
+        
+        if hasattr(user, 'client') and instance.sender_client == user.client:
+            instance.delete()
+        elif hasattr(user, 'employee') and instance.sender_employee == user.employee:
+            instance.delete()
+        else:
+            raise PermissionDenied("You do not have permission to delete this message.")
+        
+    
+        
+        
